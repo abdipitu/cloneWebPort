bd0e2bdc36b7de13d151d508aa7403cc14424b61
abdipitu <demitkipuw@gmail.com>
2024-01-31 05:07:20 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'outfit': ['Outfit', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
